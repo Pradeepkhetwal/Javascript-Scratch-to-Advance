ebb52236561494e6e0ce3d3dfa78e02b4348d421
Pradeepkhetwal <pradeepkhetwal077@gmail.com>
2025-02-10 19:29:06 +0530

    dom starting

---
+++ 08_Document_Object_Model/01.js
@@ -0,0 +1,7 @@
+//printing the document object.
+//you can see in the console it contains all the details of the webpage.
+// console.log(window.document); we can write this line as
+console.log(document)//as window object is present by default so it will automatically link with window object.
+
+//accessing an html element by it's id.Using document object method.
+const username = console.log(document.getElementById("username")); //it will return an object of that element whose id is username. so we can store it in a const.
