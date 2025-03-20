784ab16e80c634511298d76a323b97a79a910fdf
Pradeepkhetwal <pradeepkhetwal077@gmail.com>
2025-03-20 12:38:43 +0530

    completed fetch

---
+++ 12_Fetch/01_fetch.js
@@ -0,0 +1,37 @@
+//fetch is also used to make api calls.
+
+// It is the new way of making api calls.
+
+const URL = "https://jsonplaceholder.typicode.com/posts";
+
+//fetch k andar we need to pass the url (jaha humne api call karni hai).
+
+//fetch by default get request karta hai .
+
+//fetch hamesha promise return karta hai means we can directly use then with it.
+
+//ab jo ye promise return hota hai agar tum iske response ko directly print karte ho like console.log(response) to you will not get the response data directly instead tumhe ek response object milega with lot of properties so ab iss object mein se actual data ko pane k liye we have .json() function jo ki response data de deta hai in json format.
+
+//agar fetch resolve hoga to then chalega. par fetch sirf network error ane par hi resolve hota hai.
+// which means ki agar server error like in 500 codes or client error like in 400's code to uss case mein fetch reject nahi hota hai to catch block run hi ni hota , wo sirf network error ane pe hi run hota hai.
+
+//so to resolve it we have a property in the response object that is response.ok ab iski value true hoti hai jab sab theek ho aur agar koi error aa jati hai to us case mein iski value false ho jati hai so isi ko use karenge hum , jaise hi iski value false hai means there is some error or mistake and we will thow an error and due to this throwing of error catch block bhi run ho jayega.
+
+fetch(URL).then(response => {
+  // console.log("Hello");
+  // console.log(response);
+  // console.log(typeof response);
+
+  
+  if (response.ok) {
+    //this is also a promise whose resolve value is response.json(it is the acutal data);
+    return response.json();
+  } else {
+    throw new Error("Something error");
+  }
+}).then(data => {
+  console.log(data);
+}).catch(error => {
+  console.log("inside catch");
+  console.log(error);
+})
---
+++ 12_Fetch/02_Other_methods_in_fetch.js
@@ -0,0 +1,42 @@
+const URL = "https://jsonplaceholder.typicode.com/posts";
+
+//now andar jo concept hai like handling promise is same the only minor change here while make the get request is the we need to additionaly pass an extra parameter in the form of an object inside the fetch();
+
+//here method:'POST' specifies we want to make post request(it can be any request of your choice by default it is get so in that you don't explicitly define this extra parameter).
+
+//As we are making a post request so we have to send data to the server or we can say we will create a new data in the server.
+// so The body property below is used to send the data you want to include in the request.
+//json.stringify function is used to convert the data which we are sending into json format because in post method server is most likely to accept the data in the json format.
+
+//Headers are used to  provide additional metadata about the request.
+/* 'Content-type': 'application/json; charset=UTF-8' tells the server that the request body is in JSON format and uses UTF-8 encoding.
+
+Content-type: Describes the type of content you're sending. Here, it's indicating that the body of the request is JSON (application/json).
+
+charset=UTF-8: This specifies that the character encoding for the request body is UTF-8, which is the standard for encoding text.
+*/
+
+
+fetch(URL, {
+  method: 'POST',
+  body: JSON.stringify({
+    //this is the data that we are sending.
+    title: 'foo',
+    body: 'bar',
+    userId:1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+}).then(response => {
+  if (response.ok) {
+    return response.json()
+  } else {
+    throw new Error("Something went wrong");
+  }
+}).then(data => {
+  console.log(data);
+}).catch(error => {
+  console.log("inside catch");
+  console.log(error);
+})
